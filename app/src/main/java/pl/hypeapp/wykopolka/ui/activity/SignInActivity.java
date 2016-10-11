57db0bab358eb7554a02e16e6d2076756ec2720e
Przemek <pszem.szym@gmail.com>
2016-10-11 13:43:49 +0200

    Implemented simple talking to wykopolka webapp

--- app/src/main/java/pl/hypeapp/wykopolka/ui/activity/SignInActivity.java
+++ app/src/main/java/pl/hypeapp/wykopolka/ui/activity/SignInActivity.java
@@ -14,10 +14,20 @@ import com.pascalwelsch.compositeandroid.activity.CompositeActivity;
 import net.grandcentrix.thirtyinch.internal.TiPresenterProvider;
 import net.grandcentrix.thirtyinch.plugin.TiActivityPlugin;
 
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
 import pl.hypeapp.wykopolka.BuildConfig;
 import pl.hypeapp.wykopolka.R;
+import pl.hypeapp.wykopolka.WykopolkaApi;
+import pl.hypeapp.wykopolka.model.Book;
 import pl.hypeapp.wykopolka.presenter.SignInPresenter;
 import pl.hypeapp.wykopolka.view.SignInView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class SignInActivity extends CompositeActivity implements SignInView {
     private final String APP_KEY = BuildConfig.APP_KEY;
@@ -43,8 +53,64 @@ public class SignInActivity extends CompositeActivity implements SignInView {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSignInPresenter = mPresenterPlugin.getPresenter();
-        initWebClient();
+//        mSignInPresenter = mPresenterPlugin.getPresenter();
+//        initWebClient();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.1.9/")
+                .addConverterFactory(JacksonConverterFactory.create())
+                .build();
+        WykopolkaApi wykopolkaApi = retrofit.create(WykopolkaApi.class);
+
+        Call<Book[]> talkJuby = wykopolkaApi.talkToMe("5");
+//        talkJuby.enqueue(new Callback<ResponseBody>() {
+//            @Override
+//            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+////                Log.e("response string body ",  response.body().toString());
+//
+//                try {
+//                    Log.e("response model ", "" + String.valueOf(response.code() + response.body().string()));
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                Log.e("response code ", "a " + String.valueOf(response.code()));
+////                Log.e("response book ", response.body().getAuthor().toString());
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<ResponseBody> call, Throwable t) {
+//                Log.e("response failure ", t.getMessage());
+//            }
+//        });
+        talkJuby.enqueue(new Callback<Book[]>() {
+            @Override
+            public void onResponse(Call<Book[]> call, Response<Book[]> response) {
+                Log.e("response  ", " " +  response.body()[0].getAuthor());
+            }
+
+            @Override
+            public void onFailure(Call<Book[]> call, Throwable t) {
+                Log.e("response failure ", t.getMessage());
+            }
+        });
+//        talkJuby.enqueue(new Callback<ResponseBody>() {
+//            @Override
+//            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+//
+//                try {
+//                    Log.e("response body " , " " + response.code() + " " + response.body().string());
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<ResponseBody> call, Throwable t) {
+//
+//            }
+//        });
     }
 
     private void initWebClient() {
