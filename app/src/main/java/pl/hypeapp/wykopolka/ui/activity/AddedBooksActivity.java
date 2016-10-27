77b6001b4b39533a56ef9161787f2b7c4f9cf393
Przemek <pszem.szym@gmail.com>
2016-10-27 22:32:23 +0200

    Added recycler added books view

--- app/src/main/java/pl/hypeapp/wykopolka/ui/activity/AddedBooksActivity.java
+++ app/src/main/java/pl/hypeapp/wykopolka/ui/activity/AddedBooksActivity.java
@@ -3,8 +3,8 @@ package pl.hypeapp.wykopolka.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import com.pascalwelsch.compositeandroid.activity.CompositeActivity;
@@ -12,12 +12,25 @@ import com.pascalwelsch.compositeandroid.activity.CompositeActivity;
 import net.grandcentrix.thirtyinch.internal.TiPresenterProvider;
 import net.grandcentrix.thirtyinch.plugin.TiActivityPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter;
 import pl.hypeapp.wykopolka.R;
+import pl.hypeapp.wykopolka.adapter.BooksRecyclerAdapter;
+import pl.hypeapp.wykopolka.model.BookView;
+import pl.hypeapp.wykopolka.plugin.NavigationDrawerActivityPlugin;
 import pl.hypeapp.wykopolka.presenter.AddedBooksPresenter;
-import pl.hypeapp.wykopolka.ui.fragment.NavigationDrawerFragment;
 import pl.hypeapp.wykopolka.view.AddedBooksView;
 
 public class AddedBooksActivity extends CompositeActivity implements AddedBooksView {
+    BooksRecyclerAdapter mRecyclerAdapter;
+    @BindView(R.id.book_list)
+    RecyclerView mRecyclerView;
+    static String[] mTitles;
+    private final NavigationDrawerActivityPlugin mNavigationDrawerPlugin = new NavigationDrawerActivityPlugin();
     private final TiActivityPlugin<AddedBooksPresenter, AddedBooksView> mPresenterPlugin =
             new TiActivityPlugin<>(new TiPresenterProvider<AddedBooksPresenter>() {
                 @NonNull
@@ -27,21 +40,50 @@ public class AddedBooksActivity extends CompositeActivity implements AddedBooksV
                 }
             });
 
+    public static List<BookView> getData() {
+        List<BookView> dataSet = new ArrayList<>();
+
+        String[] titles = mTitles;
+
+        for (int i = 0; i < titles.length; i++) {
+            BookView current = new BookView();
+            current.title = "Awantury na tle powszechnego ciążenia";
+            current.author = "Tomasz Lem";
+            current.thumbnail = R.drawable.books;
+            dataSet.add(current);
+        }
+        return dataSet;
+    }
+
     public AddedBooksActivity() {
         addPlugin(mPresenterPlugin);
+        addPlugin(mNavigationDrawerPlugin);
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_added_books);
+        ButterKnife.bind(this);
+
+        Toolbar toolbar = initToolbar();
+        mNavigationDrawerPlugin.setNavigationDrawer(toolbar);
+        initRecyclerAdapter();
+    }
+
+    private Toolbar initToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("Wykopółka");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
-//                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
-//        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        drawerFragment.create(drawerLayout, toolbar, R.id.fragment_navigation_drawer);
+        return toolbar;
+    }
+
+    private void initRecyclerAdapter() {
+        mTitles = getResources().getStringArray(R.array.navigation_drawer_items);
+        mRecyclerAdapter = new BooksRecyclerAdapter(this, getData());
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ScaleInAnimationAdapter scaleInAnimationAdapter = new ScaleInAnimationAdapter(mRecyclerAdapter);
+        scaleInAnimationAdapter.setFirstOnly(false);
+        mRecyclerView.setAdapter(scaleInAnimationAdapter);
     }
 }
