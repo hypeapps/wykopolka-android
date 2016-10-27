cb956c69716939c0f2abc7fbabe730fb79477ef1
Przemek <pszem.szym@gmail.com>
2016-10-27 22:32:42 +0200

    Added handling intents

--- app/src/main/java/pl/hypeapp/wykopolka/ui/fragment/NavigationDrawerFragment.java
+++ app/src/main/java/pl/hypeapp/wykopolka/ui/fragment/NavigationDrawerFragment.java
@@ -1,5 +1,7 @@
 package pl.hypeapp.wykopolka.ui.fragment;
 
+import android.content.Intent;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,11 +10,11 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
@@ -26,15 +28,18 @@ import butterknife.ButterKnife;
 import pl.hypeapp.wykopolka.App;
 import pl.hypeapp.wykopolka.R;
 import pl.hypeapp.wykopolka.adapter.DrawerRecyclerAdapter;
-import pl.hypeapp.wykopolka.model.NavigationOption;
+import pl.hypeapp.wykopolka.model.NavigationItem;
 import pl.hypeapp.wykopolka.presenter.NavigationDrawerPresenter;
-import pl.hypeapp.wykopolka.ui.activity.MainWallActivity;
+import pl.hypeapp.wykopolka.ui.activity.AddedBooksActivity;
+import pl.hypeapp.wykopolka.ui.activity.DashboardActivity;
 import pl.hypeapp.wykopolka.util.transformation.CropCircleTransformation;
 import pl.hypeapp.wykopolka.view.NavigationDrawerView;
 
 public class NavigationDrawerFragment extends TiFragment<NavigationDrawerPresenter, NavigationDrawerView>
         implements NavigationDrawerView {
     public static final String KEY_USER_LEARNED_DRAWER = "user_learned_drawer";
+    private static String[] navigationDrawerTitles;
+    private static TypedArray navigationDrawerIcons;
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
     private boolean mUserLearnedDrawer;
@@ -42,10 +47,14 @@ public class NavigationDrawerFragment extends TiFragment<NavigationDrawerPresent
     private View mContainer;
     private boolean mDrawerOpened = false;
     private DrawerRecyclerAdapter mRecyclerAdapter;
-
+    private String mAvatarUrl;
+    private String mUserLogin;
+    @BindView(R.id.drawer_list)
     RecyclerView mRecyclerView;
     @BindView(R.id.iv_avatar)
     ImageView avatar;
+    @BindView(R.id.tv_username)
+    TextView username;
 
     public NavigationDrawerFragment() {
     }
@@ -56,30 +65,15 @@ public class NavigationDrawerFragment extends TiFragment<NavigationDrawerPresent
         return new NavigationDrawerPresenter();
     }
 
-    public static List<NavigationOption> getData() {
-        List<NavigationOption> data = new ArrayList<>();
-        int[] icons = {R.drawable.ic_dashboard_white_36dp,
-                R.drawable.ic_search_white_36dp,
-                R.drawable.ic_shuffle,
-                R.drawable.ic_book_white_36dp,
-                R.drawable.ic_date_range_white_36dp,
-                R.drawable.ic_shuffle,
-                R.drawable.ic_favorite_white_36dp,
-                R.drawable.ic_equalizer_white_36dp};
+    public static List<NavigationItem> getData() {
+        List<NavigationItem> data = new ArrayList<>();
+        TypedArray icons = navigationDrawerIcons;
+        String[] titles = navigationDrawerTitles;
 
-        String[] titles = {"Dashboard",
-                "Szukaj książki",
-                "Losuj książkę",
-                "Moje książki",
-                "Oddane książki",
-                "Moja historia",
-                "Lista życzeń",
-                "Ranking i statystyki"};
-
-        for (int i = 0; i < titles.length && i < icons.length; i++) {
-            NavigationOption current = new NavigationOption();
+        for (int i = 0; i < titles.length && i < icons.length(); i++) {
+            NavigationItem current = new NavigationItem();
             current.title = titles[i];
-            current.iconId = icons[i];
+            current.iconId = icons.getResourceId(i, 0);
             data.add(current);
         }
         return data;
@@ -89,25 +83,28 @@ public class NavigationDrawerFragment extends TiFragment<NavigationDrawerPresent
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mUserLearnedDrawer = App.readFromPreferences(getActivity(), KEY_USER_LEARNED_DRAWER, false);
+        mAvatarUrl = App.readFromPreferences(getActivity(), "user_avatar", "");
+        mUserLogin = App.readFromPreferences(getActivity(), "user_login", "unknown");
         mFromSavedInstanceState = savedInstanceState != null;
+        navigationDrawerIcons = getResources().obtainTypedArray(R.array.navigation_drawer_icons);
+        navigationDrawerTitles = getResources().getStringArray(R.array.navigation_drawer_items);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
         ButterKnife.bind(this, view);
-        mRecyclerView = (RecyclerView) view.findViewById(R.id.drawer_list);
-        mRecyclerAdapter = new DrawerRecyclerAdapter(getActivity(), getData());
-        mRecyclerView.setAdapter(mRecyclerAdapter);
+        mRecyclerAdapter = new DrawerRecyclerAdapter(getActivity(), getData(), new ClickItemHandler());
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(mRecyclerAdapter);
         return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Glide.with(getContext()).load(R.drawable.avatar).bitmapTransform(new CropCircleTransformation(getContext())).into(avatar);
+        Glide.with(getContext()).load(mAvatarUrl).bitmapTransform(new CropCircleTransformation(getContext())).into(avatar);
+        username.setText("@" + mUserLogin);
     }
 
     public void create(DrawerLayout drawerLayout, final Toolbar toolbar, int fragmentId) {
@@ -117,7 +114,6 @@ public class NavigationDrawerFragment extends TiFragment<NavigationDrawerPresent
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-                Log.d("OnNavgationFrag", "onDrawerOpened");
                 if (!mUserLearnedDrawer) {
                     mUserLearnedDrawer = true;
                     App.saveToPreferences(getActivity(), KEY_USER_LEARNED_DRAWER, mUserLearnedDrawer);
@@ -128,7 +124,6 @@ public class NavigationDrawerFragment extends TiFragment<NavigationDrawerPresent
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
-                Log.d("VIVZ", "onDrawerClosed");
                 getActivity().supportInvalidateOptionsMenu();
             }
 
@@ -139,18 +134,55 @@ public class NavigationDrawerFragment extends TiFragment<NavigationDrawerPresent
                 toolbar.setAlpha(1 - slideOffset / 2);
             }
         };
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
         mDrawerLayout.post(new Runnable() {
             @Override
             public void run() {
                 mDrawerToggle.syncState();
-                if (!mFromSavedInstanceState) {
+                if (!mUserLearnedDrawer && !mFromSavedInstanceState) {
                     mDrawerLayout.openDrawer(mContainer);
-                    if (getActivity() instanceof MainWallActivity && !mUserLearnedDrawer) {
-                        mDrawerLayout.openDrawer(mContainer);
-                    }
                 }
             }
         });
+    }
+
+    public class ClickItemHandler {
+
+        public void handleIntent(final int position) {
+            mDrawerLayout.closeDrawers();
+            mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+                @Override
+                public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                }
+
+                @Override
+                public void onDrawerOpened(View drawerView) {
+
+                }
+
+                @Override
+                public void onDrawerClosed(View drawerView) {
+                    startIntentActivity(position);
+                }
+
+                @Override
+                public void onDrawerStateChanged(int newState) {
+
+                }
+            });
+
+        }
+
+        private void startIntentActivity(int position) {
+            switch (position) {
+                case 0:
+                    startActivity(new Intent(getActivity(), DashboardActivity.class));
+                    break;
+                case 4:
+                    startActivity(new Intent(getActivity(), AddedBooksActivity.class));
+                    break;
+            }
+        }
     }
 }
