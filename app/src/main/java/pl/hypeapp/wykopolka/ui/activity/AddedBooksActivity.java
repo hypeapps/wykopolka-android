9c541d11529bc58df1729acd69bf366108aa3a6e
Przemek <pszem.szym@gmail.com>
2016-11-02 19:23:40 +0100

    SharedPrefrences with account key now pass to presenter. Created method to setting data do adapter form presenter

--- app/src/main/java/pl/hypeapp/wykopolka/ui/activity/AddedBooksActivity.java
+++ app/src/main/java/pl/hypeapp/wykopolka/ui/activity/AddedBooksActivity.java
@@ -12,15 +12,15 @@ import com.pascalwelsch.compositeandroid.activity.CompositeActivity;
 import net.grandcentrix.thirtyinch.internal.TiPresenterProvider;
 import net.grandcentrix.thirtyinch.plugin.TiActivityPlugin;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter;
+import pl.hypeapp.wykopolka.App;
 import pl.hypeapp.wykopolka.R;
 import pl.hypeapp.wykopolka.adapter.BooksRecyclerAdapter;
-import pl.hypeapp.wykopolka.model.BookView;
+import pl.hypeapp.wykopolka.model.Book;
 import pl.hypeapp.wykopolka.plugin.NavigationDrawerActivityPlugin;
 import pl.hypeapp.wykopolka.presenter.AddedBooksPresenter;
 import pl.hypeapp.wykopolka.view.AddedBooksView;
@@ -30,30 +30,19 @@ public class AddedBooksActivity extends CompositeActivity implements AddedBooksV
     @BindView(R.id.book_list)
     RecyclerView mRecyclerView;
     static String[] mTitles;
+    private AddedBooksPresenter addedBooksPresenter;
+
     private final NavigationDrawerActivityPlugin mNavigationDrawerPlugin = new NavigationDrawerActivityPlugin();
     private final TiActivityPlugin<AddedBooksPresenter, AddedBooksView> mPresenterPlugin =
             new TiActivityPlugin<>(new TiPresenterProvider<AddedBooksPresenter>() {
                 @NonNull
                 @Override
                 public AddedBooksPresenter providePresenter() {
-                    return new AddedBooksPresenter();
+                    String accountKey = App.readFromPreferences(AddedBooksActivity.this, "user_account_key", null);
+                    addedBooksPresenter = new AddedBooksPresenter(accountKey);
+                    return addedBooksPresenter;
                 }
             });
-
-    public static List<BookView> getData() {
-        List<BookView> dataSet = new ArrayList<>();
-
-        String[] titles = mTitles;
-
-        for (int i = 0; i < titles.length; i++) {
-            BookView current = new BookView();
-            current.title = "Awantury na tle powszechnego ciążenia";
-            current.author = "Tomasz Lem";
-            current.thumbnail = R.drawable.books;
-            dataSet.add(current);
-        }
-        return dataSet;
-    }
 
     public AddedBooksActivity() {
         addPlugin(mPresenterPlugin);
@@ -80,10 +69,16 @@ public class AddedBooksActivity extends CompositeActivity implements AddedBooksV
 
     private void initRecyclerAdapter() {
         mTitles = getResources().getStringArray(R.array.navigation_drawer_items);
-        mRecyclerAdapter = new BooksRecyclerAdapter(this, getData());
+        mRecyclerAdapter = new BooksRecyclerAdapter(this);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         ScaleInAnimationAdapter scaleInAnimationAdapter = new ScaleInAnimationAdapter(mRecyclerAdapter);
         scaleInAnimationAdapter.setFirstOnly(false);
         mRecyclerView.setAdapter(scaleInAnimationAdapter);
     }
+
+    @Override
+    public void setDataToAdapter(List<Book> books) {
+        mRecyclerAdapter.setData(books);
+    }
+
 }
