7eb872d04a758f4f014cfa43eeedef9e16cab970
Przemek <pszem.szym@gmail.com>
2016-11-02 19:24:38 +0100

    Added interfaces method

--- app/src/main/java/pl/hypeapp/wykopolka/view/SignInView.java
+++ app/src/main/java/pl/hypeapp/wykopolka/view/SignInView.java
@@ -2,5 +2,12 @@ package pl.hypeapp.wykopolka.view;
 
 import net.grandcentrix.thirtyinch.TiView;
 
+import pl.hypeapp.wykopolka.model.User;
+import retrofit2.Response;
+
 public interface SignInView extends TiView {
+
+    void saveAndLoginUser(Response<User> responseUser, String accountKey);
+
+    void showSuccessLoginInfo();
 }
