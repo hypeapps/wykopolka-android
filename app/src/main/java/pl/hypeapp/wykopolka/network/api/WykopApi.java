06a1ca9855ccfd88018739a8b9edd8fd2f5cdc52
Przemek <pszem.szym@gmail.com>
2016-11-03 18:50:14 +0100

    Removed unused code and changed interface user to Observable

--- app/src/main/java/pl/hypeapp/wykopolka/network/api/WykopApi.java
+++ app/src/main/java/pl/hypeapp/wykopolka/network/api/WykopApi.java
@@ -1,30 +1,19 @@
 package pl.hypeapp.wykopolka.network.api;
 
 import pl.hypeapp.wykopolka.model.User;
-import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
+import rx.Observable;
 
 
 public interface WykopApi {
 
-//    @GET("user/login/appkey/{appkey}/login/{username}/accountkey/{accountkey}")
-//    Call<ResponseBody> loginUser(@Path("appkey") String appkey, @Path("username") String username,
-//                                 @Path("accountkey") String accountkey, @Header("apisign") String sign);
-
-//    @GET("user/login/appkey/{appkey}/login/{username}/accountkey/{accountkey}")
-//    Call<ResponseBody> loginUser(@Path("appkey") String appkey, @Path("username") String username,
-//                                 @Path("accountkey") String accountkey);
-//
-//    @POST("user/login/appkey/{appkey}/format/json/output/clear/")
-//    Call<ResponseBody> loginUser(@Path("appkey") String appkey, @Body User user, @Header("apisign") String sign);
-
     @FormUrlEncoded
     @POST("user/login/appkey/{appkey}/format/json/output/clear/")
-    Call<User> loginUser(@Path("appkey") String appkey, @Field("login") String username,
-            @Field("accountkey") String accountkey, @Header("apisign") String sign);
+    Observable<User> loginUser(@Path("appkey") String appkey, @Field("login") String username,
+                               @Field("accountkey") String accountkey, @Header("apisign") String sign);
 }
 
