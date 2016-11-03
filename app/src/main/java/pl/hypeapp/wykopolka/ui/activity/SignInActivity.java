6614c24357df49851d8a9d87cd7bd66d1e5f283f
Przemek <pszem.szym@gmail.com>
2016-11-03 18:49:17 +0100

    Implemented SignInView methods and renamed and recreated method for save and login user.

--- app/src/main/java/pl/hypeapp/wykopolka/ui/activity/SignInActivity.java
+++ app/src/main/java/pl/hypeapp/wykopolka/ui/activity/SignInActivity.java
@@ -6,35 +6,35 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import com.pascalwelsch.compositeandroid.activity.CompositeActivity;
 
 import net.grandcentrix.thirtyinch.internal.TiPresenterProvider;
 import net.grandcentrix.thirtyinch.plugin.TiActivityPlugin;
 
-import java.io.IOException;
-
-import okhttp3.ResponseBody;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import pl.hypeapp.wykopolka.App;
 import pl.hypeapp.wykopolka.BuildConfig;
 import pl.hypeapp.wykopolka.R;
-import pl.hypeapp.wykopolka.WykopolkaApi;
-import pl.hypeapp.wykopolka.model.Book;
+import pl.hypeapp.wykopolka.model.User;
 import pl.hypeapp.wykopolka.presenter.SignInPresenter;
 import pl.hypeapp.wykopolka.view.SignInView;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class SignInActivity extends CompositeActivity implements SignInView {
-    private final String APP_KEY = BuildConfig.APP_KEY;
-    private final String CONNECT_SUCCESS_URL = "https://a.wykop.pl/user/ConnectSuccess/";
-    private final String CONNECT_WYKOP_API_URL = "http://a.wykop.pl/user/connect/appkey/";
-    private final String WYKOP_API_URL = "a.wykop.pl";
+    private static final String APP_KEY = BuildConfig.APP_KEY;
+    private static final String CONNECT_SUCCESS_URL = "https://a.wykop.pl/user/ConnectSuccess/";
+    private static final String CONNECT_WYKOP_API_URL = "http://a.wykop.pl/user/connect/appkey/";
+    private static final String WYKOP_API_URL = "a.wykop.pl";
     private SignInPresenter mSignInPresenter;
+    @BindView(R.id.webview)
+    WebView loginWebView;
+    @BindView(R.id.tv_connect_success)
+    TextView successLoginTextView;
 
     private final TiActivityPlugin<SignInPresenter, SignInView> mPresenterPlugin =
             new TiActivityPlugin<>(new TiPresenterProvider<SignInPresenter>() {
@@ -52,72 +52,45 @@ public class SignInActivity extends CompositeActivity implements SignInView {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//        mSignInPresenter = mPresenterPlugin.getPresenter();
-//        initWebClient();
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.1.9/")
-                .addConverterFactory(JacksonConverterFactory.create())
-                .build();
-        WykopolkaApi wykopolkaApi = retrofit.create(WykopolkaApi.class);
-
-        Call<Book[]> talkJuby = wykopolkaApi.talkToMe("5");
-//        talkJuby.enqueue(new Callback<ResponseBody>() {
-//            @Override
-//            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-////                Log.e("response string body ",  response.body().toString());
-//
-//                try {
-//                    Log.e("response model ", "" + String.valueOf(response.code() + response.body().string()));
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//
-//                Log.e("response code ", "a " + String.valueOf(response.code()));
-////                Log.e("response book ", response.body().getAuthor().toString());
-//
-//            }
-//
-//            @Override
-//            public void onFailure(Call<ResponseBody> call, Throwable t) {
-//                Log.e("response failure ", t.getMessage());
-//            }
-//        });
-        talkJuby.enqueue(new Callback<Book[]>() {
-            @Override
-            public void onResponse(Call<Book[]> call, Response<Book[]> response) {
-                Log.e("response  ", " " +  response.body()[0].getAuthor());
-            }
-
-            @Override
-            public void onFailure(Call<Book[]> call, Throwable t) {
-                Log.e("response failure ", t.getMessage());
-            }
-        });
-//        talkJuby.enqueue(new Callback<ResponseBody>() {
-//            @Override
-//            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-//
-//                try {
-//                    Log.e("response body " , " " + response.code() + " " + response.body().string());
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//
-//            }
-//
-//            @Override
-//            public void onFailure(Call<ResponseBody> call, Throwable t) {
-//
-//            }
-//        });
+        setContentView(R.layout.activity_sign_in);
+        ButterKnife.bind(this);
+        mSignInPresenter = mPresenterPlugin.getPresenter();
+        initWebClient();
     }
 
     private void initWebClient() {
         WebClient webClient = new WebClient();
-        WebView loginWebView = (WebView) findViewById(R.id.webview);
         loginWebView.setWebViewClient(webClient);
+        //Test on API > 18
+        loginWebView.getSettings().setSavePassword(false);
         loginWebView.loadUrl(CONNECT_WYKOP_API_URL + APP_KEY);
+    }
+
+    @Override
+    public void loginAndSaveUser(User user, String accountKey) {
+        Log.e("SignInActivity", accountKey);
+        Log.e("SignInActivity", user.getLogin());
+        Log.e("SignInActivity", user.getAvatar());
+        App.saveToPreferences(this, "user_account_key", accountKey);
+        App.saveToPreferences(this, "user_login", user.getLogin());
+        App.saveToPreferences(this, "user_avatar", user.getAvatarBig());
+        startActivity(new Intent(this, DashboardActivity.class));
+    }
+
+    @Override
+    public void showSuccessLoginInfo() {
+        loginWebView.setVisibility(View.GONE);
+        successLoginTextView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void showError() {
+
     }
 
     class WebClient extends WebViewClient {
