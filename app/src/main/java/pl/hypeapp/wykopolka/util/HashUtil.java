2c7e5a93600e7a482a84dae8db84ff634c33c734
Przemek <pszem.szym@gmail.com>
2016-11-15 23:47:24 +0100

    Created new method for signing API

--- app/src/main/java/pl/hypeapp/wykopolka/util/HashUtil.java
+++ app/src/main/java/pl/hypeapp/wykopolka/util/HashUtil.java
@@ -1,11 +1,19 @@
 package pl.hypeapp.wykopolka.util;
 
+import android.util.Log;
+
+import org.apache.commons.lang3.StringUtils;
+
 import java.math.BigInteger;
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import pl.hypeapp.wykopolka.BuildConfig;
+
 public class HashUtil {
+    private static final String WYKOPOLKA_SECRET = BuildConfig.WYKOPOLKA_SECRET;
+
     public static String md5(String s) {
         MessageDigest digest;
         try {
@@ -19,5 +27,12 @@ public class HashUtil {
             e.printStackTrace();
         }
         return "";
+    }
+
+    public static String generateApiSign(String... parameters) {
+        String parametersImploded = StringUtils.join(parameters, ",");
+        Log.e("STRING UTILS ", parametersImploded);
+        String apiSign = md5(WYKOPOLKA_SECRET + parametersImploded);
+        return apiSign;
     }
 }
