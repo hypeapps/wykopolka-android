f0b01736a041591f3968d36d32ea307107c5ca09
Przemek <pszem.szym@gmail.com>
2016-11-18 21:01:51 +0100

    Created Book panel pager adapter

---
+++ app/src/main/java/pl/hypeapp/wykopolka/adapter/BookPanelPagerAdapter.java
@@ -0,0 +1,44 @@
+package pl.hypeapp.wykopolka.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import pl.hypeapp.wykopolka.ui.fragment.AddedBooksFragment;
+import pl.hypeapp.wykopolka.ui.fragment.MyBooksFragment;
+
+public class BookPanelPagerAdapter extends FragmentPagerAdapter {
+
+    public BookPanelPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new MyBooksFragment();
+            case 1:
+                return new AddedBooksFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "moje książki";
+            case 1:
+                return "oddane książki";
+            default:
+                return null;
+        }
+    }
+}
