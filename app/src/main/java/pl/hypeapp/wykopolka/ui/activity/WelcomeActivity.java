aee66b0ab2b39497aa11a4b567ec3ce7ed2ca459
Przemek <pszem.szym@gmail.com>
2016-11-25 01:34:58 +0100

    Added methods for nextButton

--- app/src/main/java/pl/hypeapp/wykopolka/ui/activity/WelcomeActivity.java
+++ app/src/main/java/pl/hypeapp/wykopolka/ui/activity/WelcomeActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.merhold.extensiblepageindicator.ExtensiblePageIndicator;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import pl.hypeapp.wykopolka.R;
 import pl.hypeapp.wykopolka.adapter.WelcomePagerAdapter;
 import pl.hypeapp.wykopolka.extra.pagetransformer.StackTransformer;
@@ -16,6 +19,7 @@ import pl.hypeapp.wykopolka.extra.pagetransformer.StackTransformer;
 public class WelcomeActivity extends AppCompatActivity {
     @BindView(R.id.viewpager) ViewPager mViewPager;
     @BindView(R.id.flexibleIndicator) ExtensiblePageIndicator mFlexibleIndicator;
+    @BindView(R.id.iv_next) ImageView mNextPageButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,7 +32,38 @@ public class WelcomeActivity extends AppCompatActivity {
     private void initViewPager(ViewPager viewPager) {
         WelcomePagerAdapter welcomePagerAdapter = new WelcomePagerAdapter(getSupportFragmentManager());
         viewPager.setPageTransformer(true, new StackTransformer());
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (mNextPageButton != null) {
+                    manageButtonVisibility(position);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
         viewPager.setAdapter(welcomePagerAdapter);
         mFlexibleIndicator.initViewPager(mViewPager);
+    }
+
+    private void manageButtonVisibility(int position) {
+        if (position > 1) {
+            mNextPageButton.setVisibility(View.GONE);
+        } else {
+            mNextPageButton.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @OnClick(R.id.iv_next)
+    public void nextPage() {
+        mViewPager.setCurrentItem(mViewPager.getCurrentItem() + 1);
     }
 }
