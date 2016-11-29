5a6bde3ab349334d15e8a1d674636f54d5477019
Przemek <pszem.szym@gmail.com>
2016-11-29 21:38:18 +0100

    Renamed and added initToolbar method

--- app/src/main/java/pl/hypeapp/wykopolka/plugin/ToolbarActivityPlugin.java
+++ app/src/main/java/pl/hypeapp/wykopolka/plugin/ToolbarActivityPlugin.java
@@ -1,6 +1,7 @@
 package pl.hypeapp.wykopolka.plugin;
 
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 
 import com.pascalwelsch.compositeandroid.activity.ActivityPlugin;
@@ -8,12 +9,23 @@ import com.pascalwelsch.compositeandroid.activity.ActivityPlugin;
 import pl.hypeapp.wykopolka.R;
 import pl.hypeapp.wykopolka.ui.fragment.NavigationDrawerFragment;
 
-public class NavigationDrawerActivityPlugin extends ActivityPlugin {
+public class ToolbarActivityPlugin extends ActivityPlugin {
+    private Toolbar mToolbar;
 
     public void setNavigationDrawer(Toolbar toolbar){
         NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
               getCompositeDelegate().getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         DrawerLayout drawerLayout = (DrawerLayout) getActivity().findViewById(R.id.drawer_layout);
         drawerFragment.create(drawerLayout, toolbar, R.id.fragment_navigation_drawer);
+    }
+
+    public Toolbar initToolbar(Toolbar toolbar){
+        mToolbar = toolbar;
+        getCompositeDelegate().setSupportActionBar(mToolbar);
+        ActionBar actionBar = getCompositeDelegate().getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        return mToolbar;
     }
 }
