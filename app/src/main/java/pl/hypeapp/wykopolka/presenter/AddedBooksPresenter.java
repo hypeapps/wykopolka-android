9fe8b73508dda28748d69f64e41614e4c312dce4
Przemek <pszem.szym@gmail.com>
2016-12-05 21:57:35 +0100

    Refactor/reformat

--- app/src/main/java/pl/hypeapp/wykopolka/presenter/AddedBooksPresenter.java
+++ app/src/main/java/pl/hypeapp/wykopolka/presenter/AddedBooksPresenter.java
@@ -42,80 +42,12 @@ public class AddedBooksPresenter extends TiPresenter<BookListView> {
         mRetrofitComponent = DaggerRetrofitComponent.builder().build();
         mRetrofitComponent.inject(this);
         Log.e("base_URL", mRetrofit.baseUrl().toString());
-        testApi();
     }
 
     @Override
     protected void onWakeUp() {
         super.onWakeUp();
         loadAddedBooks(mAccountKey);
-    }
-
-
-    private void testApi() {
-//        String apiSign = HashUtil.generateApiSign(mAccountKey);
-//        WykopolkaApi wykopolkaApi = mRetrofit.create(WykopolkaApi.class);
-//        Call<ResponseBody> call = wykopolkaApi.getMyBooks(mAccountKey, apiSign);
-//        call.enqueue(new Callback<ResponseBody>() {
-//            @Override
-//            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-//                try {
-//                    Log.d("response", "onResponse: " + response.body().string());
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<ResponseBody> call, Throwable t) {
-//
-//            }
-//        });
-//        Call<ResponseBody> call = wykopolkaApi.getRandomBook(mAccountKey, apiSign);
-//        call.enqueue(new Callback<ResponseBody>() {
-//            @Override
-//            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-//                try {
-//                    Log.e("asd", "onResponse: " +  response.body().string());
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<ResponseBody> call, Throwable t) {
-//
-//            }
-//        });
-//        Call<ResponseBody> call = wykopolkaApi.getSelectedBooks(mAccountKey, "2", apiSign);
-//        call.enqueue(new Callback<ResponseBody>() {
-//            @Override
-//            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-////                Log.e("message",  " " + String.valueOf(response.code()));
-//                try {
-//                    Log.e("selected ", response.body().string());
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<ResponseBody> call, Throwable t) {
-//
-//            }
-//        });
-//        Call<Statistics> call = wykopolkaApi.getGlobalStats(mAccountKey, apiSign);
-//        call.enqueue(new Callback<Statistics>() {
-//            @Override
-//            public void onResponse(Call<Statistics> call, Response<Statistics> response) {
-//                Log.e("stats", " " + response.body().getStatsGetMostWantedBook());
-//            }
-//
-//            @Override
-//            public void onFailure(Call<Statistics> call, Throwable t) {
-//
-//            }
-//        });
     }
 
     private void loadAddedBooks(String accountKey) {
