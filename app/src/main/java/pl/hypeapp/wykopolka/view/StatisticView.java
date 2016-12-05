6568b177dd3f15c603118a875992c48b3c9dc1e4
Przemek <pszem.szym@gmail.com>
2016-12-05 18:57:10 +0100

    Add: statistic view interface

--- app/src/main/java/pl/hypeapp/wykopolka/view/StatisticView.java
+++ app/src/main/java/pl/hypeapp/wykopolka/view/StatisticView.java
@@ -1,6 +1,30 @@
 package pl.hypeapp.wykopolka.view;
 
 import net.grandcentrix.thirtyinch.TiView;
+import net.grandcentrix.thirtyinch.callonmainthread.CallOnMainThread;
+
+import pl.hypeapp.wykopolka.model.Statistics;
 
 public interface StatisticView extends TiView {
+
+    @CallOnMainThread
+    void showStatistics(Statistics statistics);
+
+    @CallOnMainThread
+    void hideStatistics();
+
+    @CallOnMainThread
+    void startLoadingAnimation();
+
+    @CallOnMainThread
+    void stopLoadingAnimation();
+
+    @CallOnMainThread
+    void showError();
+
+    @CallOnMainThread
+    void hideError();
+
+    @CallOnMainThread
+    void retry();
 }
