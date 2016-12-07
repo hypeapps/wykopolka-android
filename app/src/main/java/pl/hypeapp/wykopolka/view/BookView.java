b485cba3f5d3ac26585dab70d456d0d4dc759d35
Przemek <pszem.szym@gmail.com>
2016-12-07 22:44:06 +0100

    Add: Book view method sigantures

--- app/src/main/java/pl/hypeapp/wykopolka/view/BookView.java
+++ app/src/main/java/pl/hypeapp/wykopolka/view/BookView.java
@@ -1,26 +1,49 @@
 package pl.hypeapp.wykopolka.view;
 
 import net.grandcentrix.thirtyinch.TiView;
+import net.grandcentrix.thirtyinch.callonmainthread.CallOnMainThread;
+import net.grandcentrix.thirtyinch.distinctuntilchanged.DistinctUntilChanged;
 
 import pl.hypeapp.wykopolka.model.Book;
 
 public interface BookView extends TiView {
 
-    void setBookInfo(Book book);
+    @DistinctUntilChanged
+    @CallOnMainThread
+    void setBookDescription(Book book);
 
-    void setBookOwner(String addedBy, String ownedBy);
+    @DistinctUntilChanged
+    @CallOnMainThread
+    void setBookHolders(String addedBy, String ownedBy);
 
-    void setWishlistStatus(boolean status);
-
+    @DistinctUntilChanged
+    @CallOnMainThread
     void setBookCover(String coverUrl);
 
+    @CallOnMainThread
+    void setWishStatusWilled();
+
+    @CallOnMainThread
+    void setWishStatusNotWilled();
+
+    @CallOnMainThread
+    void setWishIconDisabled();
+
+    @CallOnMainThread
     void animateCardEnter();
 
+    @CallOnMainThread
+    void animateFabButtonEnter();
+
+    @CallOnMainThread
     void loadPdfBookCard();
 
+    @CallOnMainThread
     void editBookDescription();
 
+    @CallOnMainThread
     void addBookToWishlist();
 
+    @CallOnMainThread
     void showSnackbarBookWishlistedSuccesful();
 }
