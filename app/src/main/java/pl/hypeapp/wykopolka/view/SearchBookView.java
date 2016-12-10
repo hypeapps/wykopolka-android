325e8f1cf26e64e12984131f082b4655d8029631
Przemek <pszem.szym@gmail.com>
2016-12-10 21:57:09 +0100

    Add: interfaces for search book view

--- app/src/main/java/pl/hypeapp/wykopolka/view/SearchBookView.java
+++ app/src/main/java/pl/hypeapp/wykopolka/view/SearchBookView.java
@@ -1,6 +1,7 @@
 package pl.hypeapp.wykopolka.view;
 
 import net.grandcentrix.thirtyinch.TiView;
+import net.grandcentrix.thirtyinch.callonmainthread.CallOnMainThread;
 
 import java.util.List;
 
@@ -8,10 +9,37 @@ import pl.hypeapp.wykopolka.model.Book;
 
 public interface SearchBookView extends TiView {
 
+    @CallOnMainThread
     void setSearchedBooksByTitle(List<Book> books);
 
+    @CallOnMainThread
     void setSearchedBooksByAuthor(List<Book> books);
 
+    @CallOnMainThread
     void setSearchedBooksByGenre(List<Book> books);
+
+    @CallOnMainThread
+    void resetResults();
+
+    @CallOnMainThread
+    void showSearchFailed();
+
+    @CallOnMainThread
+    void hideSearchFailed();
+
+    @CallOnMainThread
+    void showLoading();
+
+    @CallOnMainThread
+    void hideLoading();
+
+    @CallOnMainThread
+    void retry();
+
+    @CallOnMainThread
+    void showError();
+
+    @CallOnMainThread
+    void hideError();
 
 }
