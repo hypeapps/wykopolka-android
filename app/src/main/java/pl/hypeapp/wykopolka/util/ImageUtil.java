dede9d2184f00c3767fa5d8a5666af2f11ba37cd
Przemek <pszem.szym@gmail.com>
2016-12-12 23:30:44 +0100

    Add: method that decoding bitmap to drawable

--- app/src/main/java/pl/hypeapp/wykopolka/util/ImageUtil.java
+++ app/src/main/java/pl/hypeapp/wykopolka/util/ImageUtil.java
@@ -3,6 +3,7 @@ package pl.hypeapp.wykopolka.util;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Base64;
 
 import java.io.ByteArrayOutputStream;
 
@@ -18,5 +19,11 @@ public class ImageUtil {
     public static Bitmap decodeBitmap(byte[] bytes) {
         Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
         return bitmap;
+    }
+
+    public static String encodeToBase64(Bitmap image, Bitmap.CompressFormat compressFormat, int quality) {
+        ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
+        image.compress(compressFormat, quality, byteArrayOS);
+        return Base64.encodeToString(byteArrayOS.toByteArray(), Base64.DEFAULT);
     }
 }
