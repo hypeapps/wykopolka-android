0176406e95e5b1012bf19f046491f6bd77fd38d1
Przemek <pszem.szym@gmail.com>
2016-12-16 20:12:53 +0100

    Add: provider class

---
+++ app/src/main/java/pl/hypeapp/wykopolka/provider/FileContentProvider.java
@@ -0,0 +1,84 @@
+package pl.hypeapp.wykopolka.provider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.support.annotation.Nullable;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+
+
+public class FileContentProvider extends ContentProvider {
+    public static final Uri CONTENT_URI = Uri.parse("content://com.example.camerademo/");
+    private static final HashMap<String, String> MIME_TYPES = new HashMap<String, String>();
+
+    static {
+        MIME_TYPES.put(".jpg", "image/jpeg");
+        MIME_TYPES.put(".jpeg", "image/jpeg");
+    }
+
+    @Override
+    public boolean onCreate() {
+        try {
+            File mFile = new File(getContext().getFilesDir(), "newImage.jpg");
+            if (!mFile.exists()) {
+                mFile.createNewFile();
+            }
+            getContext().getContentResolver().notifyChange(CONTENT_URI, null);
+            return (true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        throw new RuntimeException("Operation not supported");
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        String path = uri.toString();
+        for (String extension : MIME_TYPES.keySet()) {
+            if (path.endsWith(extension)) {
+                return (MIME_TYPES.get(extension));
+            }
+        }
+        return (null);
+    }
+
+    @Nullable
+    @Override
+    public ParcelFileDescriptor openFile(Uri uri, String mode) throws FileNotFoundException {
+        File f = new File(getContext().getFilesDir(), "newImage.jpg");
+        if (f.exists()) {
+            return (ParcelFileDescriptor.open(f,
+                    ParcelFileDescriptor.MODE_READ_WRITE));
+        }
+        throw new FileNotFoundException(uri.getPath());
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        throw new RuntimeException("Operation not supported");
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        throw new RuntimeException("Operation not supported");
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        throw new RuntimeException("Operation not supported");
+    }
+}
