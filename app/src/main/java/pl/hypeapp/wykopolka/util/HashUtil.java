045cf7b6890633676b508534fc93764f16b885a0
Przemek <pszem.szym@gmail.com>
2016-12-21 23:51:57 +0100

    Fix: new md5 method

--- app/src/main/java/pl/hypeapp/wykopolka/util/HashUtil.java
+++ app/src/main/java/pl/hypeapp/wykopolka/util/HashUtil.java
@@ -2,8 +2,7 @@ package pl.hypeapp.wykopolka.util;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.math.BigInteger;
-import java.nio.charset.Charset;
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -12,19 +11,22 @@ import pl.hypeapp.wykopolka.BuildConfig;
 public class HashUtil {
     private static final String WYKOPOLKA_SECRET = BuildConfig.WYKOPOLKA_SECRET;
 
-    public static String md5(String s) {
-        MessageDigest digest;
+    public static String md5(String string) {
+        byte[] hash;
         try {
-            digest = MessageDigest.getInstance("MD5");
-            digest.update(s.getBytes(Charset.forName("US-ASCII")), 0, s.length());
-            byte[] magnitude = digest.digest();
-            BigInteger bi = new BigInteger(1, magnitude);
-            String hash = String.format("%0" + (magnitude.length << 1) + "x", bi);
-            return hash;
+            hash = MessageDigest.getInstance("MD5").digest(string.getBytes("UTF-8"));
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Huh, MD5 should be supported?", e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("Huh, UTF-8 should be supported?", e);
         }
-        return "";
+        StringBuilder hex = new StringBuilder(hash.length * 2);
+        for (byte b : hash) {
+            int i = (b & 0xFF);
+            if (i < 0x10) hex.append('0');
+            hex.append(Integer.toHexString(i));
+        }
+        return hex.toString();
     }
 
     public static String generateApiSign(String... parameters) {
