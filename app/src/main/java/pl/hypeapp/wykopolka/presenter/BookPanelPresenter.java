7157cdae44a2659d28b8e8d4e1b777d349e24049
Przemek <pszem.szym@gmail.com>
2016-12-27 18:50:03 +0100

    Implemented setPager for adapter

--- app/src/main/java/pl/hypeapp/wykopolka/presenter/BookPanelPresenter.java
+++ app/src/main/java/pl/hypeapp/wykopolka/presenter/BookPanelPresenter.java
@@ -5,4 +5,17 @@ import net.grandcentrix.thirtyinch.TiPresenter;
 import pl.hypeapp.wykopolka.view.BookPanelView;
 
 public class BookPanelPresenter extends TiPresenter<BookPanelView> {
+    private int mPage;
+
+    public BookPanelPresenter(int page) {
+        mPage = page;
+    }
+
+    @Override
+    protected void onWakeUp() {
+        super.onWakeUp();
+        if (getView() != null) {
+            getView().setPage(mPage);
+        }
+    }
 }
--- app/src/main/java/pl/hypeapp/wykopolka/view/BookPanelView.java
+++ app/src/main/java/pl/hypeapp/wykopolka/view/BookPanelView.java
@@ -1,6 +1,13 @@
 package pl.hypeapp.wykopolka.view;
 
 import net.grandcentrix.thirtyinch.TiView;
+import net.grandcentrix.thirtyinch.callonmainthread.CallOnMainThread;
 
 public interface BookPanelView extends TiView {
+
+    @CallOnMainThread
+    void addBook();
+
+    @CallOnMainThread
+    void setPage(int page);
 }
