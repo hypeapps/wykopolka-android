d99d69c1cf8a49dca9e0feb4780a1beda41b3d0c
Przemek <pszem.szym@gmail.com>
2016-12-29 20:13:53 +0100

    Added util and implemented view function needed to working with backend

--- app/src/main/java/pl/hypeapp/wykopolka/ui/activity/ProfileSettingsActivity.java
+++ app/src/main/java/pl/hypeapp/wykopolka/ui/activity/ProfileSettingsActivity.java
@@ -1,8 +1,10 @@
 package pl.hypeapp.wykopolka.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
@@ -14,6 +16,9 @@ import com.pascalwelsch.compositeandroid.activity.CompositeActivity;
 import net.grandcentrix.thirtyinch.internal.TiPresenterProvider;
 import net.grandcentrix.thirtyinch.plugin.TiActivityPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -22,16 +27,19 @@ import pl.hypeapp.wykopolka.R;
 import pl.hypeapp.wykopolka.model.User;
 import pl.hypeapp.wykopolka.plugin.ToolbarActivityPlugin;
 import pl.hypeapp.wykopolka.presenter.ProfileSettingsPresenter;
+import pl.hypeapp.wykopolka.util.WebViewUtil;
 import pl.hypeapp.wykopolka.view.ProfileSettingsView;
 
 public class ProfileSettingsActivity extends CompositeActivity implements ProfileSettingsView {
     private ProfileSettingsPresenter mProfileSettingsPresenter;
     private final ToolbarActivityPlugin mToolbarPlugin = new ToolbarActivityPlugin();
     @BindView(R.id.toolbar) Toolbar mToolbar;
+    @BindView(R.id.settings_layout) View parentLayout;
     @BindView(R.id.et_settings_fullname) EditText mSettingsFullName;
     @BindView(R.id.et_settings_address) EditText mSettingsAddress;
     @BindView(R.id.et_settings_postal_code) EditText mSettingsPostalCode;
     @BindView(R.id.et_settings_city) EditText mSettingsCity;
+    @BindView(R.id.et_settings_address_email) EditText mSettingsEmail;
     @BindView(R.id.profile_settings) LinearLayout mProfileSettingsLayout;
     @BindView(R.id.switch_notification_email) Switch mSwitchNotificationEmail;
     @BindView(R.id.switch_notification_pm) Switch mSwitchNotificationPm;
@@ -65,7 +73,7 @@ public class ProfileSettingsActivity extends CompositeActivity implements Profil
     }
 
     @Override
-    public void setAddress(User user) {
+    public void setProfileAddress(User user) {
         mSettingsFullName.setText(user.getFullName());
         mSettingsAddress.setText(user.getAdress());
         mSettingsCity.setText(user.getCity());
@@ -73,14 +81,89 @@ public class ProfileSettingsActivity extends CompositeActivity implements Profil
     }
 
     @Override
-    public void setSettings(User userSettings) {
+    public void setProfileEmail(User user) {
+        mSettingsEmail.setText(user.getMail());
+    }
 
+    @Override
+    public void setEmailNotificationsOn() {
+        mSwitchNotificationEmail.setChecked(true);
+    }
+
+    @Override
+    public void setEmailNotificationsOff() {
+        mSwitchNotificationEmail.setChecked(false);
+    }
+
+    @Override
+    public void setEmailNotificationsEnabled() {
+        mSwitchNotificationEmail.setEnabled(true);
+    }
+
+    @Override
+    public void setEmailNotificationsDisabled() {
+        mSwitchNotificationEmail.setEnabled(false);
+    }
+
+    @Override
+    public void setPmNotificationsOn() {
+        mSwitchNotificationPm.setChecked(true);
+    }
+
+    @Override
+    public void setPmNotificationsOff() {
+        mSwitchNotificationPm.setChecked(false);
     }
 
     @Override
     @OnClick(R.id.btn_save_settings)
     public void submitSettings() {
+        mProfileSettingsPresenter.submitSettings();
+    }
 
+    @Override
+    public List<String> getProfileAddress() {
+        List<String> profileAddress = new ArrayList<>();
+        profileAddress.add(mSettingsFullName.getText().toString().trim());
+        profileAddress.add(mSettingsAddress.getText().toString().trim());
+        profileAddress.add(mSettingsPostalCode.getText().toString().trim());
+        profileAddress.add(mSettingsCity.getText().toString().trim());
+        return profileAddress;
+    }
+
+    @Override
+    public String getProfileEmail() {
+        return mSettingsEmail.getText().toString().trim();
+    }
+
+    @Override
+    public boolean getCheckedEmail() {
+        return mSwitchNotificationEmail.isChecked();
+    }
+
+    @Override
+    public boolean getCheckedPm() {
+        return mSwitchNotificationPm.isChecked();
+    }
+
+    @Override
+    public void showMessageAddressIncorrect() {
+        Snackbar.make(parentLayout, R.string.error_message_wrong_address, Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void showMessageAddressEmailIncorrect() {
+        Snackbar.make(parentLayout, R.string.error_message_wrong_email, Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void showMessageSaveSettingsFail() {
+        Snackbar.make(parentLayout, R.string.error_message_default, Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void showMessageSaveSettingsSuccess() {
+        Snackbar.make(parentLayout, R.string.success_message_user_update, Snackbar.LENGTH_LONG).show();
     }
 
     @Override
@@ -128,13 +211,17 @@ public class ProfileSettingsActivity extends CompositeActivity implements Profil
     @Override
     @OnClick(R.id.btn_error_retry)
     public void retry() {
-        mProfileSettingsPresenter.loadSettingsAgain();
+        mProfileSettingsPresenter.retryLoadProfileSettings();
     }
 
     @Override
     @OnClick(R.id.btn_logout)
     public void logout() {
-
+        App.clearPreferences(this);
+        WebViewUtil.clearCookies(this);
+        Intent intentToWelcomeScreen = new Intent(this, WelcomeActivity.class);
+        intentToWelcomeScreen.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intentToWelcomeScreen);
     }
 
 }
--- app/src/main/java/pl/hypeapp/wykopolka/view/ProfileSettingsView.java
+++ app/src/main/java/pl/hypeapp/wykopolka/view/ProfileSettingsView.java
@@ -1,31 +1,83 @@
 package pl.hypeapp.wykopolka.view;
 
 import net.grandcentrix.thirtyinch.TiView;
+import net.grandcentrix.thirtyinch.callonmainthread.CallOnMainThread;
+
+import java.util.List;
 
 import pl.hypeapp.wykopolka.model.User;
 
 public interface ProfileSettingsView extends TiView {
 
-    void setAddress(User user);
+    @CallOnMainThread
+    void setProfileAddress(User user);
 
-    void setSettings(User userSettings);
+    @CallOnMainThread
+    void setProfileEmail(User user);
 
+    @CallOnMainThread
+    void setEmailNotificationsOn();
+
+    @CallOnMainThread
+    void setEmailNotificationsOff();
+
+    @CallOnMainThread
+    void setEmailNotificationsEnabled();
+
+    @CallOnMainThread
+    void setEmailNotificationsDisabled();
+
+    @CallOnMainThread
+    void setPmNotificationsOn();
+
+    @CallOnMainThread
+    void setPmNotificationsOff();
+
+    List<String> getProfileAddress();
+
+    String getProfileEmail();
+
+    boolean getCheckedEmail();
+
+    boolean getCheckedPm();
+
+    @CallOnMainThread
+    void showMessageAddressIncorrect();
+
+    @CallOnMainThread
+    void showMessageAddressEmailIncorrect();
+
+    @CallOnMainThread
+    void showMessageSaveSettingsFail();
+
+    @CallOnMainThread
+    void showMessageSaveSettingsSuccess();
+
+    @CallOnMainThread
     void submitSettings();
 
+    @CallOnMainThread
     void showSettingsCard();
 
+    @CallOnMainThread
     void hideSettingsCard();
 
+    @CallOnMainThread
     void showError();
 
+    @CallOnMainThread
     void hideError();
 
+    @CallOnMainThread
     void showLoading();
 
+    @CallOnMainThread
     void hideLoading();
 
+    @CallOnMainThread
     void retry();
 
+    @CallOnMainThread
     void logout();
 
 }
